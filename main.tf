6f78bc3e997849a7ea05d0d0b1bf458789d73b97
Cristian Boarna <cristian.boarna@gmail.com>
2020-05-10 17:02:29 +0300

    fix(lambda): Added lambda layer passthrough

--- main.tf
+++ main.tf
@@ -40,6 +40,7 @@ module "lambda" {
   lambda_memory_size                    = var.lambda_memory_size
   lambda_vpc_security_group_ids         = var.lambda_vpc_security_group_ids
   lambda_vpc_subnet_ids                 = var.lambda_vpc_subnet_ids
+  lambda_layers                         = var.lambda_layers
 
   #Internal
   lambda_role                           = module.iam.lambda_role_arn
